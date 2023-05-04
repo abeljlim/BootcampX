c964f14bce099ed4751ad278ba8d85731e09e3dc
abeljlim <subrosian@hotmail.com>
2023-05-03 21:32:16 -0700

    Create students.js and implement some querying before removing commented-out code

---
+++ bootcamp_app/students.js
@@ -0,0 +1,33 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: 'labber',
+  password: '123',
+  host: 'localhost',
+  database: 'bootcampx'
+});
+
+/* pool.query(`
+SELECT id, name, cohort_id
+FROM students
+LIMIT 5;
+`)
+.then(res => {
+  console.log(res.rows);
+})
+.catch(err => console.error('query error', err.stack)); */
+pool.query(`
+SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+FROM students
+JOIN cohorts ON cohort_id = cohorts.id
+LIMIT 5;
+`)
+.then(res => {
+  res.rows.forEach(record => {
+    console.log(`${record.name} has an id of ${record.student_id} and was in the ${record.cohort} cohort`);
+  })
+});
+/* .then(res => {
+  console.log(res.rows);
+})
+.catch(err => console.error('query error', err.stack)); */
