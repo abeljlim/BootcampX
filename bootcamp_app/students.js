b18e64aa6f57255bd7878462b084d16cdfa072ab
abeljlim <subrosian@hotmail.com>
2023-05-03 22:25:29 -0700

    Use parameterized queries for student.js and teacher.js

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -9,13 +9,20 @@ const pool = new Pool({
 
 const args = process.argv.slice(2);
 
-pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-FROM students
-INNER JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '${args[0]}%'
-LIMIT ${args[1]};
-`)
+const queryString = `
+  SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+  FROM students
+  JOIN cohorts ON cohorts.id = cohort_id
+  WHERE cohorts.name LIKE $1
+  LIMIT $2;
+  `;
+const cohortName = process.argv[2];
+const limit = process.argv[3] || 5;
+// Store all potentially malicious values in an array.
+const values = [`%${cohortName}%`, limit];
+
+
+pool.query(queryString, values)
 .then(res => {
   res.rows.forEach(record => {
     console.log(`${record.name} has an id of ${record.student_id} and was in the ${record.cohort} cohort`);
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -15,9 +15,9 @@ FROM teachers
 JOIN assistance_requests ON teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${args[0] || 'JUL02'}'
+WHERE cohorts.name = $1
 ORDER BY teacher;
-`)
+`, [(args[0] || 'JUL02')])
 .then(res => {
   res.rows.forEach(record => {
     console.log(`${record.cohort}: ${record.teacher}`);
