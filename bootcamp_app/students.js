eaa114bde48df9ca80ed68d67bc62addec741311
abeljlim <subrosian@hotmail.com>
2023-05-03 21:48:45 -0700

    Write PostgreSQL connection code and querying

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -7,14 +7,30 @@ const pool = new Pool({
   database: 'bootcampx'
 });
 
+const args = process.argv.slice(2);
+
 pool.query(`
 SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
 FROM students
-JOIN cohorts ON cohort_id = cohorts.id
-LIMIT 5;
+INNER JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '${args[0]}%'
+LIMIT ${args[1]};
 `)
 .then(res => {
   res.rows.forEach(record => {
     console.log(`${record.name} has an id of ${record.student_id} and was in the ${record.cohort} cohort`);
   })
 });
+/* // This (which does query interpolation the way Andy Lindsay showed us) doesn't work for some reason
+pool.query(`
+SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
+FROM students
+INNER JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name LIKE '$1%'
+LIMIT $2;
+`, args)
+.then(res => {
+  res.rows.forEach(record => {
+    console.log(`${record.name} has an id of ${record.student_id} and was in the ${record.cohort} cohort`);
+  })
+}); */
