515f3ab13696964201982391292755b1eb283ad7
abeljlim <subrosian@hotmail.com>
2023-05-03 22:25:48 -0700

    Clean out commented code

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -28,16 +28,3 @@ pool.query(queryString, values)
     console.log(`${record.name} has an id of ${record.student_id} and was in the ${record.cohort} cohort`);
   })
 });
-/* // This (which does query interpolation the way Andy Lindsay showed us) doesn't work for some reason
-pool.query(`
-SELECT students.id AS student_id, students.name AS name, cohorts.name AS cohort
-FROM students
-INNER JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name LIKE '$1%'
-LIMIT $2;
-`, args)
-.then(res => {
-  res.rows.forEach(record => {
-    console.log(`${record.name} has an id of ${record.student_id} and was in the ${record.cohort} cohort`);
-  })
-}); */
